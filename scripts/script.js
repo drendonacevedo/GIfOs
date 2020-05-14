636cc3baf3c23e5423a830c42138fb462a24b6b4
drendonacevedo <48222579+drendonacevedo@users.noreply.github.com>
2020-05-13 22:06:42 -0500

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -106,7 +106,7 @@ function getSearchResults() {
     var valorBusqueda = document.getElementById('busqueda').value;
     var search = valorBusqueda;
     const found =
-    fetch('http://api.giphy.com/v1/gifs/search?q=' + search +
+    fetch('https://api.giphy.com/v1/gifs/search?q=' + search +
     '&api_key=' + apiKey)
     .then((response) => {
     return response.json()
